ed8a169d9a35387ee9c6e2e23181f0342ceb26e1
OBM9 <TheLeviathan12@hotmail.com>
2025-07-04 21:18:46 -0700

    commit

--- hello.py
+++ hello.py
@@ -11,8 +11,12 @@ score = 0
 answer = input("What does gpu stand for? ")
 if answer.lower() == "graphical processing unit":
     print("Correct!")
+    score += 1
+    score = score + 1
 else:
     print("Incorrect!")
+    score -= 1
+    score + score - 1
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
     print("Correct!")
