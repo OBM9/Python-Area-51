9ddcd76cab40dfe2f1f31a97e9e2cc215be55e58
OBM9 <TheLeviathan12@hotmail.com>
2025-07-05 02:30:06 -0700

    commit

--- hello.py
+++ hello.py
@@ -37,5 +37,5 @@ else:
     print("Incorrect!")
     
 print("You got " + str(score) + " questions correct!")
-print("You got " + str((score/4) * 100) + "%.")
+print("You got " + str((score/5) * 100) + "%.")
     
