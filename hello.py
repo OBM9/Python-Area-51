1463c29ae0e82d3c8d960deb4b24c1d2d21cf4af
OBM9 <TheLeviathan12@hotmail.com>
2025-07-04 17:57:04 -0700

    commit

--- hello.py
+++ hello.py
@@ -1,28 +1,28 @@
 print("Welcome to my computer quiz!")
 
-playing = input("Do you want to play?")
+playing = input("Do you want to play? ")
 
 if playing.lower() != "yes":
     quit()
     
 print("Okay! Let's play!")    
 
-answer = input("What does gpu mean")
+answer = input("What does gpu stand for? ")
 if answer() == "Graphical processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
-answer = input("What does RAM stand for?")
+answer = input("What does RAM stand for? ")
 if answer() == "random access memory":
     print("Correct!")
 else:
     print("Incorrect!")
-answer = input("What does CPU stand for?")
+answer = input("What does CPU stand for? ")
 if answer() == "central processing unit":
     print("Correct!")
 else:
     print("Incorrect!")
-answer = input("What does PSU stand for?")
+answer = input("What does PSU stand for? ")
 if answer() == "power supply unit":
     print("Correct!")
 else:
