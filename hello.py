76708a7d6991f9b02ad757ebd1ddf356669aa257
OBM9 <TheLeviathan12@hotmail.com>
2025-07-10 02:11:27 -0700

    commit

--- hello.py
+++
@@ -1,6 +0,0 @@
-name_of_user = input("Enter your name? ")
-length_of_name = len(name_of_user)
-print(type("Number of letters in your name"))
-print(str(length_of_name))
-
-print("Number of letters in your name: " + str(length_of_name))
