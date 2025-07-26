d298455cf3c819e65e719f0e4cda7ae24cc4094b
OBM9 <TheLeviathan12@hotmail.com>
2025-07-26 01:49:44 -0700

    commit

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 print("Welcome to the tip calculator!!")
 bill = float(input("What was the total bill? $"))
-tip = input("What percentage tip would you like to give? 10 12 15")
+tip = int(input("What percentage tip would you like to give? 10 12 15"))
