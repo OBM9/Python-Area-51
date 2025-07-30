f6941226bea3c22b84d7092e7ee647059e2d41e3
OBM9 <TheLeviathan12@hotmail.com>
2025-07-30 02:53:47 -0700

    commit

--- hello.py
+++ hello.py
@@ -3,9 +3,8 @@ height = int(input("What is your height? "))
 
 if height > 120:
     print("You can ride the rollercoaster")
-else: < 120:
-    print("You cannot ride the rollercoaster")
-
+else: 
+    
     
     
 
