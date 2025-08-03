bf5bb919a1a71afcc3c774e358fd92616eda53a4
OBM9 <TheLeviathan12@hotmail.com>
2025-08-03 00:08:40 -0700

    commit

--- hello.py
+++ hello.py
@@ -17,7 +17,7 @@ if height >= 120:
             
     wants_photo = input("Do you want a photo take? Type Y for yes and n for No.")
     if wants_photo == "y":
-            bill = bill + 3
+            bill += 3
             
     print(f"Your final bill is {bill}")        
             
