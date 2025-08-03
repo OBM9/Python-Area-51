9f0e7a6b7084e3246a61debab6111d7826490329
OBM9 <TheLeviathan12@hotmail.com>
2025-08-03 00:03:30 -0700

    commit

--- hello.py
+++ hello.py
@@ -16,7 +16,7 @@ if height >= 120:
             print("Adult tickets are $12.")
             
     wants_photo = input("Do you want a photo take? Type Y for yes and n for No.")
-        if wants_photo == "y":
+    if wants_photo == "y":
             bill = bill + 3
             
         print(f"Your final bill is {bill}")
