7d0769a6073a3ec171aa102356a1ee57cb25486e
OBM9 <TheLeviathan12@hotmail.com>
2025-08-05 09:05:21 -0700

    commit

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L:")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N:")
+extra_cheese = input("Do")
 bill = 0
 
 if size == "S": 
