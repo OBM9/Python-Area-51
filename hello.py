42ef776807a93cbae5f680ac95bb363a92d84c3c
OBM9 <TheLeviathan12@hotmail.com>
2025-09-14 17:47:54 -0700

    commit

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 
 age = 35
 quantity = 3
+num_of_students = 45
 
 print(f"You are {age} years old." )
 print(f"You are buying {quantity} items.")
+print(f"You class has {num_of_students} students.")
