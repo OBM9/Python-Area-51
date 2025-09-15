c12ce7baf214bb527ace5e8c30908401ec905d94
OBM9 <TheLeviathan12@hotmail.com>
2025-09-14 23:08:15 -0700

    commit

--- hello.py
+++ hello.py
@@ -3,3 +3,4 @@ price = float(input("What is the price?: "))
 quantity = int(input("How many would like?: "))
 total = price * quantity
 print(f"You have bought {quantity} x {item}/2")
+print(f"Your total is: {total}")
