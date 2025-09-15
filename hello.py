63d80249fceea63941a3dc69d257d1b84d39442c
OBM9 <TheLeviathan12@hotmail.com>
2025-09-14 22:58:33 -0700

    commit

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-item = input("What item would like to buy: ")
+item = input("What item would you like to buy: ")
 price = float(input("What is the price?: "))
 quantity = int(input("How many would like?: "))
 total = price * quantity
