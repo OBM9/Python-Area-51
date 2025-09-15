00d3ac25e9debf467ff953b0223527502210d4c7
OBM9 <TheLeviathan12@hotmail.com>
2025-09-15 01:45:23 -0700

    commit

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 adjective1 = input("Enter an adjective (description): ")
 noun1 = input("Enter a noun (person, place, thing): ")
 adjective2 = input("Enter an adjective (description): ")
-verb1 = input("Enter a verb ending with 'ing'")
+verb1 = input("Enter a verb ending with 'ing' ")
 adjective3 = input("Enter an adjective (description): ")
 
 print(f"Today I went to a {adjective1} zoo.")
