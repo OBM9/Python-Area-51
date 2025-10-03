f18ab8b64223b33b4d7c6869bd0b349dfb9aec03
OBM9 <TheLeviathan12@hotmail.com>
2025-10-03 02:18:42 -0700

    commit

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ age = int(input("Enter your age: "))
 
 if age >= 100:
     print("You are too old in sign up.")
-if age>= 18:
+elif age>= 18:
     print("You are now signed up!")
 elif age < 0:
     print("You haven't been born yet.")
