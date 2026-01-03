0aae5c4860a7aac8649ba9634a631033dad09a4a
OBM9 <TheLeviathan12@hotmail.com>
2026-01-03 03:58:20 -0700

    commit

--- hello.py
+++ hello.py
@@ -5,12 +5,14 @@ unit = input("Kilograms or Pounds (K or L)")
 
 if unit == "K":
     weight = weight * 2.205
+    unit = "Lbs."
 elif unit == "L":
     weight = weight / 2.205
+    unit = "Kgs."
 else:
     print(f"{unit} was not valid")
     
-print(f"Your weight is: {weight} {}")
+print(f"Your weight is: {weight} {unit}")
 
 
 
