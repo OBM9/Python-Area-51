4c807700d9c18b1fd80e2993c655e39882f548d0
OBM9 <TheLeviathan12@hotmail.com>
2026-01-03 02:38:01 -0700

    commit

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ operator = input("Enter an operator (+ - * /): ")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-print(num1 + num2)
+
 
 if operator == '+':
     result = num1 + num2
