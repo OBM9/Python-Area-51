65333252a98df7f0bc8d15191de77acda3c5eb0c
OBM9 <TheLeviathan12@hotmail.com>
2026-01-03 02:44:31 -0700

    commit

--- hello.py
+++ hello.py
@@ -4,7 +4,6 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 
-
 if operator == '+':
     result = num1 + num2
     print(round(result, 3))
@@ -17,6 +16,9 @@ elif operator == '*':
 elif operator == '/':
     result = num1 / num2
     print(round(result, 3))
+else:
+    print(f"{operator} is not a valid")
+    
 
 
 
