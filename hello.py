5e7bdfca4ae73cc10454524c441a4f47ad45dbe9
OBM9 <TheLeviathan12@hotmail.com>
2026-01-03 02:48:38 -0700

    commit

--- hello.py
+++ hello.py
@@ -17,7 +17,7 @@ elif operator == '/':
     result = num1 / num2
     print(round(result, 3))
 else:
-    print(f"{operator} is not valid operator")
+    print(f"{operator} is not a valid operator")
     
 
 
