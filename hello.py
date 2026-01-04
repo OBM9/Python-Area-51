6999efdad53fa29054b0a277f85f32ba020cc64f
OBM9 <TheLeviathan12@hotmail.com>
2026-01-04 02:25:29 -0700

    commit

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
+unit = input("Is this temperature in Celsius or Fahrenheit (C/F)? ")
+temp = float(input("Enter the temperature: "))
 
-
+if unit == "C":
+    pass
+elif unit == "F":
+    pass
+else:
+    print(f"{unit} is an invalid unit of measurement")
 
 
 
