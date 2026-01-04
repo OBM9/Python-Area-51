244109ee421b8bedb6d3e5c6abb898565f9ea397
OBM9 <TheLeviathan12@hotmail.com>
2026-01-04 11:48:47 -0700

    commit

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@ unit = input("Is this temperature in Celsius or Fahrenheit (C/F)? ")
 temp = float(input("Enter the temperature: "))
 
 if unit == "C":
-        temp = round((9 * temp) / 5 + 32, 2)
+        temp = round((9 * temp) / 5 + 32, 1)
+        print(f"The temperature in Fahrenheit is {temp}°F")
 elif unit == "F":
-    pass
+        temp = round((temp - 32) * 5 / 9, 1)
+        print(f"The temperature in Celsius is {temp}°C")
 else:
     print(f"{unit} is an invalid unit of measurement")
 
