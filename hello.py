64d972b8b5889d43f6cce58d75ccb7c16d3743c3
OBM9 <TheLeviathan12@hotmail.com>
2026-01-04 11:32:46 -0700

    commit

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ unit = input("Is this temperature in Celsius or Fahrenheit (C/F)? ")
 temp = float(input("Enter the temperature: "))
 
 if unit == "C":
-    
+        temp = round((9 * temp) / 5 + 32, 2)
 elif unit == "F":
     pass
 else:
