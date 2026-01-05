6003bfae1d6058e9bdd8d33268fa633d5f5954a1
OBM9 <TheLeviathan12@hotmail.com>
2026-01-04 19:46:58 -0700

    commit

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ email = input("Enter your email: ")
 index = email.find("@")
 
 username  = email[:index]
-domain = email[index:]
+domain = email[index + 1:]
 
 print(f"Your username is {username} and your domain is {domain}")
 
