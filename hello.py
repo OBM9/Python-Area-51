e456404346f3e14b2046e853820113397bd17c8f
OBM9 <TheLeviathan12@hotmail.com>
2026-01-06 04:13:01 -0700

    commit

--- hello.py
+++ hello.py
@@ -11,9 +11,8 @@ while principle <= 0:
 while rate <= 0:
     rate = float(input("Enter the interest rate: "))
     if rate <=0:
-        print("Interest rate can't be less than or equal to zero")
-        
-    print(rate)
+        print("Interest rate can't be less than or equal to zero")      
+   
     
 while time <= 0:
     time = int(input("Enter the time in years: "))
