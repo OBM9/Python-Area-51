1154ed5cbf8cc32cb685bc775ea33031687612fd
OBM9 <TheLeviathan12@hotmail.com>
2026-01-06 03:24:32 -0700

    commit

--- hello.py
+++ hello.py
@@ -6,7 +6,9 @@ time = 0
 while principle <= 0:
     principle = float(input("Enter the principle amount: "))
     if principle <=0:
-        print("Principle ")
+        print("Principle can't be less than or equal to zero")
+        
+    print(principle)
 
 
 
