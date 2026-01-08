86ee12943e950ba34ce18560b87504312e193158
OBM9 <TheLeviathan12@hotmail.com>
2026-01-08 04:37:24 -0700

    commit

--- hello.py
+++ hello.py
@@ -1,8 +1,7 @@
 import time
 my_time = int(input("Enter time in seconds: "))
 
-for x in range(0, my_time):
-    print(f"Time elapsed: {x + 1} seconds")
+for x in range(0, my_time):   
 
 time.sleep(3)
 print("Time up!")
